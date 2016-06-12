0300436797bc78de1fea816d62177f303f426c28
genro <genro@DESKTOP-5Q9P0RM>
2016-06-12 17:49:45 +0200

    First commit!

---
+++ desktop/src/com/techdungeon/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.techdungeon.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.techdungeon.game.TechDungeonGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TechDungeonGame(), config);
+	}
+}
